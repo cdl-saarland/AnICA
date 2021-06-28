81c0ef99670501e6ccc1506483270bcf9f54258d
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-06-28 11:33:18 +0200

    Extract PredictorManager to its own file

--- devidisc/devidisc.py
+++ devidisc/devidisc.py
@@ -6,10 +6,8 @@
 import argparse
 from collections import defaultdict
 from datetime import datetime
-from functools import partial
 import json
 import multiprocessing
-from multiprocessing import Pool
 import os
 import pathlib
 import random
@@ -22,68 +20,11 @@ from iwho.predictors import Predictor
 from iwho.utils import parse_args_with_logging
 import iwho.x86 as x86
 
+from predmanager import PredictorManager
+
 
 import logging
 logger = logging.getLogger(__name__)
-
-
-def evaluate_bb(bb, pred):
-    try:
-        result = pred.evaluate(bb, disable_logging=True)
-    except Exception as e:
-        result = "an exception occured: " + str(e)
-    return result
-
-
-class LightBBWrapper:
-    """ Light-weight wrapper of all things necessary to get the hex
-    representation of a BasicBlock.
-
-    The key is the execution time distribution:
-    Creating this wrapper is fast, serializing it is efficient (since nothing
-    unnecessary from the iwho context is present), and `get_hex()` does the
-    heavy lifting (i.e. calls to the coder, which might require expensive
-    subprocesses).
-
-    This makes this very effective to use as a task for a PredictorManager,
-    since all expensive things can be done by the process pool.
-
-    This could be considered a hack.
-    """
-    def __init__(self, bb):
-        self.asm_str = bb.get_asm()
-        self.coder = bb.context.coder
-        self.hex = None
-
-    def get_hex(self):
-        if self.hex is None:
-            self.hex = self.coder.asm2hex(self.asm_str)
-        return self.hex
-
-    def get_asm(self):
-        return self.asm_str
-
-
-class PredictorManager:
-    def __init__(self, num_threads=None):
-        if num_threads is None:
-            num_threads = multiprocessing.cpu_count()
-        self.pool = Pool(num_threads)
-
-    def do(self, pred, bbs, lazy=True):
-        """ Use the given predictor to predict inverse throughputs for all
-        basic blocks in the given list.
-
-        If lazy is true, return an asynchronous iterator for the results and
-        return before they are all predicted.
-        If lazy is false, return a complete list of predictions (awaiting all
-        results).
-        """
-        tasks = map(lambda x: LightBBWrapper(x), bbs)
-        results = self.pool.imap(partial(evaluate_bb, pred=pred), tasks)
-        if not lazy:
-            results = list(results)
-        return results
 
 
 def main():
---
+++ devidisc/predmanager.py
@@ -0,0 +1,67 @@
+""" TODO document
+"""
+
+import multiprocessing
+from multiprocessing import Pool
+
+
+from functools import partial
+
+
+def evaluate_bb(bb, pred):
+    try:
+        result = pred.evaluate(bb, disable_logging=True)
+    except Exception as e:
+        result = "an exception occured: " + str(e)
+    return result
+
+
+class LightBBWrapper:
+    """ Light-weight wrapper of all things necessary to get the hex
+    representation of a BasicBlock.
+
+    The key is the execution time distribution:
+    Creating this wrapper is fast, serializing it is efficient (since nothing
+    unnecessary from the iwho context is present), and `get_hex()` does the
+    heavy lifting (i.e. calls to the coder, which might require expensive
+    subprocesses).
+
+    This makes this very effective to use as a task for a PredictorManager,
+    since all expensive things can be done by the process pool.
+
+    This could be considered a hack.
+    """
+    def __init__(self, bb):
+        self.asm_str = bb.get_asm()
+        self.coder = bb.context.coder
+        self.hex = None
+
+    def get_hex(self):
+        if self.hex is None:
+            self.hex = self.coder.asm2hex(self.asm_str)
+        return self.hex
+
+    def get_asm(self):
+        return self.asm_str
+
+
+class PredictorManager:
+    def __init__(self, num_threads=None):
+        if num_threads is None:
+            num_threads = multiprocessing.cpu_count()
+        self.pool = Pool(num_threads)
+
+    def do(self, pred, bbs, lazy=True):
+        """ Use the given predictor to predict inverse throughputs for all
+        basic blocks in the given list.
+
+        If lazy is true, return an asynchronous iterator for the results and
+        return before they are all predicted.
+        If lazy is false, return a complete list of predictions (awaiting all
+        results).
+        """
+        tasks = map(lambda x: LightBBWrapper(x), bbs)
+        results = self.pool.imap(partial(evaluate_bb, pred=pred), tasks)
+        if not lazy:
+            results = list(results)
+        return results
