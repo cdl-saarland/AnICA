ea6210474350d34f3b4bebda6c6bd45bff55018c
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-07-06 19:46:27 +0200

    Add a bit of a hack to nicely highlight selected abstract blocks in the interactive explorer

--- html_resources/script.js
+++ html_resources/script.js
@@ -4,10 +4,13 @@ var curr_highlighted = null;
 function clickHandler(ident, target_url) {
     if (curr_highlighted != ident) {
         if (curr_highlighted != null) {
-            document.getElementById(curr_highlighted).classList.remove("highlighted")
+            document.getElementById(curr_highlighted).style.backgroundColor = null;
         }
         curr_highlighted = ident;
-        document.getElementById(curr_highlighted).classList.add("highlighted")
+        const elem = document.getElementById(curr_highlighted)
+        const color_str = window.getComputedStyle(elem).borderColor
+        // this is a bit of a hack to make the color transparent
+        elem.style.backgroundColor = color_str.replace("rgb", "rgba").replace(")", ", 0.2)")
     }
     document.getElementById("target").innerText = target_url;
 }
