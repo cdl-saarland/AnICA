de6c33a4c523d7c916413c7a9feaca6b45dbbfbc
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-08-12 14:46:05 +0200

    Make ithemal wrapper work for concurrent calls

--- test/test_predictors.py
+++ test/test_predictors.py
@@ -3,6 +3,7 @@
 import pytest
 import os
 import sys
+import random as rand
 
 import iwho
 
@@ -42,4 +43,46 @@ def test_all():
         print(f" (time: {timer.seconds_passed:.5} seconds)")
 
     assert errors == 0
+
+
+def test_ithemal_parallel():
+    rand.seed(424242)
+    Timer.enabled = True
+
+    actx = AbstractionContext(config={})
+
+    test_bbs = [
+            make_bb(actx, "add rax, 0x2a\nsub [rbx + 0x8], rcx\nadd rax, 0x2a\nsub [rbx + 0x8], rcx"),
+            make_bb(actx, "sub [rbx + 0x8], rcx"),
+            make_bb(actx, "add rax, 0x2a\nadd rax, 0x2a"),
+            make_bb(actx, "add rax, 0x2a"),
+        ]
+
+    workload_size = 5
+    # workload_size = 20
+    workload = [rand.choice(test_bbs) for i in range(workload_size)]
+
+    actx.predmanager.set_predictors(['ithemal.bhive.skl', 'ithemal.bhive.hsw'])
+
+    pool = actx.predmanager.pool
+
+    with Timer('parallel', log=False) as timer:
+        res = actx.predmanager.eval_with_all(workload)
+        parallel_results = [x[1] for x in res]
+
+    print("\nparallel time: {}".format(timer.seconds_passed))
+
+    # this disables multiprocessing in the predmanager
+    actx.predmanager.pool = None
+
+    with Timer('sequential', log=False) as timer:
+        res = actx.predmanager.eval_with_all(workload)
+        sequential_results = [x[1] for x in res]
+
+    print("\nsequential time: {}".format(timer.seconds_passed))
+
+    for par_res, seq_res in zip(parallel_results, sequential_results):
+        assert par_res == seq_res
+
+    assert any(map(lambda x: x['ithemal.bhive.skl']['TP'] != x['ithemal.bhive.hsw']['TP'], sequential_results))
 
