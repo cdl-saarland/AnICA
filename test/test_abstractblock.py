cdl-saarland/AnICA

commit 8b2a9c227a1b001c285cdb40474097ea997a44c8

Make it possible to not use the `exact_scheme` abstraction

devidisc/insnfeaturemanager.py
@@ -150,12 +150,16 @@ class InsnFeatureManager(metaclass=ConfigMeta):
         `absfeature_dict` is a dict mapping feature names to instances of
         `AbstractFeature`, like it is found in `AbstractInsn`.
         """
-        scheme = absfeature_dict['exact_scheme'].get_val()
-        if scheme is not None:
-            # we could validate that the other features don't exclude this
-            # scheme, but that cannot be an issue as long as we only go up in
-            # the lattice
-            return {scheme}
+        exact_scheme_entry = absfeature_dict.get('exact_scheme', None)
+        if exact_scheme_entry is not None:
+            # special handling for thsi one, because there is only one feasible
+            # scheme that is trivially known
+            scheme = exact_scheme_entry.get_val()
+            if scheme is not None:
+                # we could validate that the other features don't exclude this
+                # scheme, but that cannot be an issue as long as we only go up in
+                # the lattice
+                return {scheme}
 
         feasible_schemes = None
 
@@ -265,8 +269,9 @@ class InsnFeatureManager(metaclass=ConfigMeta):
 
         res = dict()
 
-        res['exact_scheme'] = ischeme
-        remaining_features.discard('exact_scheme')
+        if 'exact_scheme' in self.features:
+            res['exact_scheme'] = ischeme
+            remaining_features.discard('exact_scheme')
 
         if 'mnemonic' in remaining_features:
             res['mnemonic'] = self.iwho_ctx.extract_mnemonic(ischeme)

devidisc/abstractblock.py
@@ -674,7 +674,8 @@ class AbstractInsn(Expandable):
         return len(feasible_schemes) / num_prev_feasible_schemes
 
     def get_possible_expansions(self):
-        if not self.features['exact_scheme'].is_top():
+        exact_scheme_entry = self.features.get('exact_scheme', None)
+        if exact_scheme_entry is not None and not exact_scheme_entry.is_top():
             # The exact scheme is more specific than the other features (it
             # implies all of them). It is therefore pointless and harmful to
             # expand another feature first, as it will not affect the sampling

commit f17847d28af75d7210d5ac1ceaa372e74580363f

Fix a bug introduced in 8b2a9c22 that broke the exact_scheme domain

devidisc/insnfeaturemanager.py
@@ -269,7 +269,7 @@ class InsnFeatureManager(metaclass=ConfigMeta):
 
         res = dict()
 
-        if 'exact_scheme' in self.features:
+        if any(map(lambda x: x[0] == 'exact_scheme', self.features)):
             res['exact_scheme'] = ischeme
             remaining_features.discard('exact_scheme')
 

test/test_abstractblock.py
@@ -27,6 +27,12 @@ def test_concrete_ab_single_insn(random, actx):
     assert ab.subsumes(ab)
 
 
+def test_concrete_ab_single_insn_not_bottom(random, actx):
+    bb = make_bb(actx, "add rax, 0x2a")
+    ab = AbstractBlock(actx, bb)
+    assert not ab.abs_insns[0].features['exact_scheme'].is_bottom()
+
+
 def test_concrete_ab_single_insn_sample(random, actx):
     bb = make_bb(actx, "add rax, 0x2a")
     ab = AbstractBlock(actx, bb)