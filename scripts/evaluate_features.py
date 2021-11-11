999964f8aacc2cbc2f2c852019c58ffc11a3412d
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-11-11 11:49:10 +0100

    Add a short script to evaluate the usefulness of individual features

---
+++ scripts/evaluate_features.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+""" For a list of campaigns, try to answer the question
+"Which abstract features where useful?".
+"""
+
+import argparse
+from collections import Counter
+import csv
+import json
+from pathlib import Path
+import textwrap
+
+import os
+import sys
+
+import multiprocessing
+from multiprocessing import Pool
+from functools import partial
+
+import_path = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(import_path)
+
+from anica.configurable import load_json_config
+from anica.abstractblock import AbstractBlock
+from anica.abstractioncontext import AbstractionContext
+from anica.satsumption import check_subsumed
+
+
+def handle_campaign(campaign_dir):
+    base_dir = Path(campaign_dir)
+    discovery_dir = base_dir / "discoveries"
+
+    metrics_path = base_dir / 'metrics.json'
+    if metrics_path.exists():
+        with open(metrics_path, 'r') as f:
+            metrics_dict = json.load(f)
+    else:
+        metrics_dict = {}
+
+    # find out about which pair of predictors this campaign is
+    campaign_config = load_json_config(base_dir / "campaign_config.json")
+    predictors = campaign_config['predictors']
+
+    assert len(predictors) == 2, "the campaign compares an unexpected number of predictors: {}".format(", ".join(map(str, predictors)))
+
+    res_str = str(predictors)
+    res_str += "\n"
+
+    all_discovery_paths = discovery_dir.glob("*.json")
+
+    all_abs = []
+
+    actx = None
+    for p in all_discovery_paths:
+        ab = AbstractBlock.load_json_dump(p, actx=actx)
+        if actx is None:
+            actx = ab.actx
+        gen_id = str(p).split('/')[-1].split('.')[0]
+        ab_metrics = metrics_dict.get(gen_id, None)
+        if ab_metrics is None:
+            print(f"Warning: no metrics found for discovery {gen_id}!")
+        else:
+            subsumed_by = ab_metrics['subsumed_by']
+            if subsumed_by is not None:
+                continue
+        all_abs.append(ab)
+
+    res_str += "num of abstract blocks: {}\n\n".format(len(all_abs))
+
+    feature_counter = Counter({k: 0 for (k, v) in actx.insn_feature_manager.features})
+
+    for ab in all_abs:
+        for ai in ab.abs_insns:
+            exact_scheme_entry = ai.features.get('exact_scheme', None)
+            if exact_scheme_entry is not None:
+                if not exact_scheme_entry.is_top():
+                    feature_counter['exact_scheme'] += 1
+                    # If the block is not uniformly interesting or it is
+                    # specific to the scheme, the other features will not even
+                    # be tried, which would make the other appear useful
+                    # although they weren't.
+                    continue
+            for k, v in ai.features.items():
+                if not v.is_top():
+                    feature_counter[k] += 1
+
+    res_str += "non-top entries per feature:\n"
+    for k, v in feature_counter.most_common():
+        res_str += f"  {k}: {v}\n"
+
+    res_str += "\n"
+
+    return res_str
+
+def main():
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument("-c", "--campaigndirs", required=True, nargs='+', metavar="PATH", help="path of the campaign directory whose discoveries should be used.")
+
+    # ap.add_argument('-o', "--output", metavar="CSVFILE", required=True, help="csv file for writing the coverage results to")
+    # ap.add_argument("infile", metavar="CSVFILE", help="csv file containing the overview results")
+
+
+    args = ap.parse_args()
+
+    num_processes = multiprocessing.cpu_count()
+    proc_pool = Pool(num_processes)
+
+    results = proc_pool.imap(handle_campaign, args.campaigndirs)
+
+    # records = []
+    for res_str in results:
+        print(res_str)
+        # records.append(res_dict)
+
+    proc_pool.close()
+
+    # with open(args.output, 'w') as f:
+    #     keys = list(records[0].keys())
+    #     writer = csv.DictWriter(f, fieldnames=keys)
+    #     writer.writeheader()
+    #     writer.writerows(records)
+
+
+if __name__ == "__main__":
+    main()
+
