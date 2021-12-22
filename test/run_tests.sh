413ea2dc23192ffa2ba2516c21966ef8a01d8c25
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-12-22 13:40:14 +0100

    Add a script to run all tests at once

---
+++ test/run_tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+IWHO_TEST="${SCRIPT_DIR}/../lib/iwho/test/test_iwho.py"
+
+set -ex
+
+pytest ${SCRIPT_DIR} ${IWHO_TEST}
