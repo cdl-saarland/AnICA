b2938ff472aebeafaa6ed4e414403943bb1e030b
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2022-07-05 16:15:36 +0200

    Document jsonrefs.py

--- anica/jsonrefs.py
+++ anica/jsonrefs.py
@@ -1,3 +1,5 @@
+""" Tools for handling json serialization of AnICA datastructures.
+"""
 
 import iwho
 
@@ -5,12 +7,45 @@ from .abstractblock import *
 
 
 class JSONReferenceManager:
-    """TODO"""
+    """ This class encapsulates means of mapping references to certain AnICA
+    objects to strings that can be mapped to meaningful references again in
+    future program executions.
+
+    We don't want to just serialize the referenced objects, because they are
+    large and usually deduplicated in the anica/iwho libraries.
+
+    The json life cycle of AnICA datastructures should be as follows:
+
+                        +----------------+
+                        | data structure |
+               +------> +----------------+ -------+
+               |                                  |
+         from_json_dict                      to_json_dict
+               |                                  |
+               +---- +----------------------+ <---+
+                     | dict with references |
+               +---> +----------------------+ ----+
+               |                                  |
+    resolve_json_references             introduce_json_references
+               |                                  |
+               +--- +------------------------+ <--+
+                    | dict with string reprs |
+               +--> +------------------------+ ---+
+               |                                  |
+        load from string                     dump as string
+               |                                  |
+               +-------- +-------------+ <--------+
+                         | string/file |
+                         +-------------+
+    """
 
     def __init__(self, iwho_ctx):
         self.iwho_ctx = iwho_ctx
 
     def introduce_json_references(self, json_dict):
+        """ Replace references to internal datatstructures in the json dict by
+        unique identifiers.
+        """
         if isinstance(json_dict, tuple) or isinstance(json_dict, list):
             return tuple((self.introduce_json_references(x) for x in json_dict))
         if isinstance(json_dict, dict):
@@ -24,6 +59,9 @@ class JSONReferenceManager:
         return json_dict
 
     def resolve_json_references(self, json_dict):
+        """ Replace the unique identifiers introduced by
+        `introduce_json_references` by references to internal datatstructures.
+        """
         if isinstance(json_dict, tuple) or isinstance(json_dict, list):
             return tuple((self.resolve_json_references(x) for x in json_dict))
         if isinstance(json_dict, dict):
